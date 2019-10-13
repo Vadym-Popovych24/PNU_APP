6a7767baa610012acf3c2f35ba324f1b725961a2
Vadym-Popovych24 <36673998+Vadym-Popovych24@users.noreply.github.com>
2019-10-13 11:30:38 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/pnu_app/activity/MainActivity.java
+++ app/src/main/java/com/example/pnu_app/activity/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         AppDatabase.destroyInstance();
         super.onDestroy();
     }*/
+    
+    
 
 
     public void listenerOnButton(){
