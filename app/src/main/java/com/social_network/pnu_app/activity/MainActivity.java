d67c920d7c691008d5aed5e1df4d0bc37412ad21
Vadym-Popovych24 <vadympopovychn24@gmail.com>
2019-10-13 15:57:41 +0000

    change owner

--- app/src/main/java/com/social_network/pnu_app/activity/MainActivity.java
+++ app/src/main/java/com/social_network/pnu_app/activity/MainActivity.java
@@ -55,8 +55,7 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(intent);
                         break;
 
-
-
+                        
                 }
             }
 
