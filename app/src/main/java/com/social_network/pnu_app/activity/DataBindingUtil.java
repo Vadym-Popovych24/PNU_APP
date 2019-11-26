08c2c01c8093c61613539bd5a23ade84c3244e2c
Vadym-Popovych24 <vadympopovychn24@gmail.com>
2019-11-26 19:46:00 +0000

    DELETE 6 classes with SQLITE_ROOM

--- app/src/main/java/com/social_network/pnu_app/activity/DataBindingUtil.java
+++
@@ -1,6 +0,0 @@
-package com.social_network.pnu_app.activity;
-
-class DataBindingUtil {
-    public static MainActivity setContentView(MainActivity mainActivity, int activity_main) {
-    return mainActivity;}
-}
--- app/src/main/java/com/social_network/pnu_app/registration/Registration.java
+++ app/src/main/java/com/social_network/pnu_app/registration/Registration.java
@@ -18,7 +18,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.social_network.pnu_app.R;
-import com.social_network.pnu_app.database.AppDatabase;
 import com.social_network.pnu_app.entity.Student;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.social_network.pnu_app.signin.SignIn;
